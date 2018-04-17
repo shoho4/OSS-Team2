7a71f9c9e007a9a1615e39b7be8cd544c28ea489
shoho4 <38120588+shoho4@users.noreply.github.com>
2018-04-18 01:15:30 +0900

    Update plus.c

--- plus.c
+++ plus.c
@@ -7,6 +7,14 @@ int main()
  int i,sum;
  for(i=1; i<=100; i++)
   
+ printf("두개의 정수덧셈");
+  
+ int a,b;
+ 
+ printf("두개의 정수입력 : ");
+ scanf("%d %d", a, b);
+ 
+ printf("%d + %d = %d\n", a, b, a+b);
  
  return 0;
  }
